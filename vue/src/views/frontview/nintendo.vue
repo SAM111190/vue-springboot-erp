9fa5d759565e27a34bdc8b16f5b3e70ae100952f
AlfredKyle <1106592673@qq.com>
2022-11-07 15:52:51 +0800

    2022.11.7 前端主页设计

--- vue/src/views/frontview/nintendo.vue
+++ vue/src/views/frontview/nintendo.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="5">
+    <el-row :gutter="0">
+      <el-col :span="4">
         <div class="card">
               <div class="img">
-                <img width="260" height="146" src="https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.2395833730697632/c_scale,w_300/ncom/en_US/games/switch/p/persona-5-royal-switch/hero">
+                <img width="216" height="121" src="https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.2395833730697632/c_scale,w_300/ncom/en_US/games/switch/p/persona-5-royal-switch/hero">
               </div>
                 <div class="title">Persona 5 Royal—Available now</div>
         </div>
       </el-col>
-      <el-col :span="5">
+      <el-col :span="4">
         <div class="card">
           <div class="img">
-            <img width="260" height="146" src="https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.2395833730697632/c_scale,w_300/ncom/en_US/games/switch/s/splatoon-3-switch/hero">
+            <img width="216" height="121" src="https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.2395833730697632/c_scale,w_300/ncom/en_US/games/switch/s/splatoon-3-switch/hero">
           </div>
           <div class="title">Splatoon 3—Available now</div>
         </div>
       </el-col>
-      <el-col :span="5">
+      <el-col :span="4">
         <div class="card">
           <div class="img">
-            <img width="260" height="146" src="https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.2395833730697632/c_scale,w_300/ncom/en_US/games/switch/b/bayonetta-3-switch/hero">
+            <img width="216" height="121" src="https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.2395833730697632/c_scale,w_300/ncom/en_US/games/switch/b/bayonetta-3-switch/hero">
           </div>
           <div class="title">Bayonetta 3—Available now</div>
         </div>
@@ -38,11 +38,12 @@ export default {
 <style scoped>
   .card {
     height: 220px;
-    width: 260px;
+    width: 216px;
     box-shadow: -1px 0px 10px 3px rgba(0, 0, 0, 0.08);
+    margin-bottom:15px;
   }
   .title {
-    padding: 10px 15px;
+    padding: 10px 25px;
     font-weight: 500;
     font-size: 1rem;
   }
