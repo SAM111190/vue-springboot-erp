fb6d87228987f985c87c95d6431722a2ed9bac2a
JHJ <3132343887@qq.com>
2022-11-08 21:24:18 +0800

    相邻算法2

---
+++ springboot/src/main/java/com/example/erp/service/IGoodsService.java
@@ -0,0 +1,16 @@
+package com.example.erp.service;
+
+import com.example.erp.entity.Goods;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author JHJ
+ * @since 2022-11-01
+ */
+public interface IGoodsService extends IService<Goods> {
+
+}
---
+++ springboot/src/main/java/com/example/erp/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.example.erp.service.impl;
+
+import com.example.erp.entity.Goods;
+import com.example.erp.mapper.GoodsMapper;
+import com.example.erp.service.IGoodsService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author JHJ
+ * @since 2022-11-01
+ */
+@Service
+public class GoodsServiceImpl extends ServiceImpl<GoodsMapper, Goods> implements IGoodsService {
+
+}
